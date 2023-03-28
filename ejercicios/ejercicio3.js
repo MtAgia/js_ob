2cad9256cfaf7e006f7134fffd397dc399baf8be
Matias G <dikimatig@gmail.com>
2023-03-28 03:04:59 -0300

    acomodando ejercicios

--- ejercicios/ejercicio3.js
+++ ejercicios/ejercicio3.js
@@ -2,3 +2,45 @@
 Escribe una clase que permita crear distintos objetos “rectángulos”, con las propiedades de alto y ancho, 
 mas los métodos necesarios para modificar y mostrar sus propiedades, calcular el perímetro y el área
 */
+class Rectangulo {
+    #id; #ancho; #alto;
+    constructor(id,alto,ancho){
+        this.#id = id 
+        this.#alto = alto
+        this.#ancho = ancho
+    };
+
+    // metodos
+    perimetro() {
+    this.perimetro = 2 * (this.alto+this.ancho);
+        document.write('<p> el perimetro del rectangulo es : ('+ this.id+ ') es '+ this.perimetro+'</p>');
+}
+    area() {
+    this.area = this.alto * this.ancho;
+    document.write('<p> el area del rectangulo es:  ('+ this.id+ ') es '+ this.area +'</p>');
+}
+
+//propiedades computadas
+get id(){
+    return this.#id;
+}
+set id(nuevoid) {
+this.#id= nuevoid;
+}
+get alto(){
+    return this.#alto;
+}
+set alto(nueva_alt) {
+this.#id= nueva_alt;
+}
+get ancho(){
+    return this.#ancho;
+}
+set ancho(nuevoancho) {
+this.#ancho= nuevoancho;
+}
+}
+
+const rectangulo_prueba = new Rectangulo(3,11,30)
+rectangulo_prueba.area()
+rectangulo_prueba.perimetro()
